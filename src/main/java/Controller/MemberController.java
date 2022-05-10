0a7665c0507b279d442621fe6c5e762ef18c0708
kato <seine.kato@rakus-partners.co.jp>
2022-05-10 14:56:27 +0900

    create membercontroller

---
+++ src/main/java/Controller/MemberController.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	@RequestMapping("")
+	public String index() {
+		return "finished";
+	}
+
+}
